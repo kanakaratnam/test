53afa0120a9e1ea7a4998f0df1f6ddd4b4ee2ff2
verdentum <verdentum@DESKTOP-BRBAM2S>
2019-12-04 12:13:55 +0530

    ss

--- src/pages/DifferentUsersLogin.java
+++ src/pages/DifferentUsersLogin.java
@@ -1,6 +1,13 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class DifferentUsersLogin 
 {
+	@FindBy(id="drlist")
+	public WebElement selectbranch;
 	
+	@FindBy(id="txtuId")
+	public WebElement uname;
 }
